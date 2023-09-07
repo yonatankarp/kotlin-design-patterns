a8f9ed52b473b49f19a236e5e176e5dfda100a5d
Yonatan Karp-Rudin <yonvata@gmail.com>
2023-09-07 06:02:45 +0200

    Setup Spotless in the repository
    This commit setting up spotless and diktat in the project, and add them as part of the CI execution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,50 @@
+plugins {
+    alias(libs.plugins.spotless) apply true
+}
+
 allprojects {
+    apply(plugin = "com.diffplug.spotless")
+
     repositories {
+        mavenLocal()
         mavenCentral()
     }
+
+    spotless {
+        kotlin {
+            target(
+                fileTree(project.rootDir) {
+                    include("**/*.kt")
+                },
+            )
+            // see https://github.com/shyiko/ktlint#standard-rules
+            trimTrailingWhitespace()
+            ktlint("0.49.0").editorConfigOverride(
+                mapOf(
+                    "ktlint_standard_package-name" to "disabled",
+                    "ktlint_standard_enum-entry-name-case" to "disabled",
+                    "ktlint_standard_trailing-comma" to "disabled",
+                    "ktlint_standard_annotation" to "disabled",
+                    "ktlint_standard_no-unused-imports" to "disabled",
+                    "ktlint_standard_trailing-comma-on-call-site" to "disabled",
+                    "ktlint_standard_trailing-comma-on-declaration-site" to "disabled",
+                ),
+            )
+            diktat("1.2.3").configFile("${rootProject.rootDir}/config/diktat/diktat-analysis.yml")
+        }
+        kotlinGradle {
+            target(
+                fileTree(".") {
+                    include("**/*.gradle.kts")
+                },
+            )
+            trimTrailingWhitespace()
+            indentWithTabs(2)
+            indentWithSpaces(4)
+            endWithNewline()
+            ktlint()
+        }
+    }
+
+    tasks.findByName("spotlessKotlinGradle")?.dependsOn("spotlessKotlin")
 }

47c7bb212ddd9b583a486fbaf34ea1e49f35f33d
Yonatan Karp-Rudin <yonvata@gmail.com>
2023-09-09 09:49:29 +0200

    Update Singleton pattern to be Kotlin idiomatic
    This commit changes the implementation of the Singleton implementation and tests to be more Kotlin idiomatic and not Java idiomatic

--- singleton/src/test/kotlin/com/yonatankarp/singleton/EnumIvoryTowerTest.kt
+++
@@ -1,9 +0,0 @@
-package com.yonatankarp.singleton
-
-/**
- * Create a new singleton test instance using the given 'getInstance' method.
- */
-internal class EnumIvoryTowerTest : SingletonTest<EnumIvoryTower> {
-    override val singletonInstanceMethod: () -> EnumIvoryTower =
-        { EnumIvoryTower.INSTANCE }
-}
